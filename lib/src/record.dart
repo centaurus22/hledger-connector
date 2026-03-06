ed0f58724026a6376f8e7da5b84477e5a95118d5
Klaus <thorres@brothersofgrey.net>
2026-03-06 14:52:16 +0100

    Document the records

--- lib/src/record.dart
+++ lib/src/record.dart
@@ -1,31 +1,56 @@
 class Transaction {
-  String description;
-  DateTime date;
-  List<SubTransaction> transactions;
+  /// An optional description
+  String? description;
 
+  /// The date of the [Transaction]
+  DateTime date;
+
+  /// A list of [SubTransaction]s, this [Transaction] is composed of.
+  List<SubTransaction> subTransactions;
+
+  /// Requires a [date], a list of [SubTransaction]s and an optional
+  /// description.
   Transaction({
-    required this.description,
+    this.description,
     required this.date,
-    required this.transactions,
+    required this.subTransactions,
   });
 }
 
+/// A [Transaction] is composed of this.
 class SubTransaction {
+  /// The [Account] the units are transferred from or too.
   Account account;
+
+  /// The amount which is transferred.
   Amount amount;
 
+  /// This requires an [Account] and an [Amount].
   SubTransaction({required this.account, required this.amount});
 }
 
+/// The amount transferred in a [SubTransaction].
+/// 
+/// In this base class the optional unit is written in front of the amount. In
+/// the child class [SuffixedAmount], the unit is written behind the amount.
 class Amount {
+  /// The amount that is transferred.
   double amount;
-  String unit;
 
-  Amount({required this.amount, required this.unit});
+  /// The optional unit.
+  String? unit;
+
+  /// The [amount] is required. Optionally a [unit] can be added.
+  Amount({required this.amount, this.unit});
 }
 
+/// Child class of the [Amount].
+/// 
+/// The difference of this and the base class ist that the unit is written after
+/// the amount.
 class SuffixedAmount extends Amount {
-  SuffixedAmount({required super.amount, required super.unit});
+  /// The [amount] is required. Optionally a [unit] can be added.
+  SuffixedAmount({required super.amount, super.unit});
 }
 
 /// This represents an account string in hledger.
