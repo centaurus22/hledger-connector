bb11ccf514089e1281d0d4fa5d98a86d36f35412
Klaus <thorres@brothersofgrey.net>
2026-03-06 13:34:03 +0100

    Complete domain for first version

--- lib/src/record.dart
+++ lib/src/record.dart
@@ -1,5 +1,35 @@
+class Transaction {
+  String description;
+  DateTime date;
+  List<SubTransaction> transactions;
+
+  Transaction({
+    required this.description,
+    required this.date,
+    required this.transactions,
+  });
+}
+
+class SubTransaction {
+  Account account;
+  Amount amount;
+
+  SubTransaction({required this.account, required this.amount});
+}
+
+class Amount {
+  double amount;
+  String unit;
+
+  Amount({required this.amount, required this.unit});
+}
+
+class SuffixedAmount extends Amount {
+  SuffixedAmount({required super.amount, required super.unit});
+}
+
 /// This represents an account string in hledger.
-/// 
+///
 /// Eg. "assets:bank:savings". The required main account name "assets" is stored
 /// in the required String [main]. Optional sub account names are stored in an
 /// optional List [sub]. In this case this would be `['bank', 'savings'].
