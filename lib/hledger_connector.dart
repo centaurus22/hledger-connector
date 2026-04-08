e0c9ef25263e513a0650c5a6f5c178b670c8e304
Klaus <thorres@brothersofgrey.net>
2026-04-08 19:36:55 +0200

    Add dartdocs to library import file

--- lib/hledger_connector.dart
+++ lib/hledger_connector.dart
@@ -1,9 +1,16 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// A dart library to connect with the hledger plain text accounting system
+/// (https://hledger.org/).
+/// 
+/// Features:
+/// * Generate journal entries and write it to a journal file with
+///   * a date and an optional description,
+///   * sub transactions between two or more accounts,
+///   * prefixed or suffixed units,
+///   * simple conversion transactions.
+/// * Conversion transactions.
+/// * Creates a new journal files if necessary.
+
 library;
 
 export 'src/hledger_connector_base.dart';
 export 'src/record.dart';
-
-// TODO: Export any libraries intended for clients of this package.
