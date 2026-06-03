84adf035bb42860a1c6486f853c10a1ed9961d20
Klaus <thorres@brothersofgrey.net>
2026-06-03 20:31:09 +0200

    Fix: Remove await from function call

--- test/write_to_file_test.dart
+++ test/write_to_file_test.dart
@@ -5,7 +5,7 @@ import 'package:hledger_connector/src/write_to_file.dart';
 
 void main() {
   test('throwing error if the file name is empty', () async {
-    Result result = await writeToFile(Success(value: ''), '');
+    Result result = writeToFile(Success(value: ''), '');
     expect(result.runtimeType, Error);
     if (result is Error) {
       expect(result.message, 'The file name cannot be empty.');
