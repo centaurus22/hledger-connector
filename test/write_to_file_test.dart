1850ea15e2d6ec5b34ec49586ce8671846219dcc
Klaus <thorres@brothersofgrey.net>
2026-06-12 12:25:37 +0200

    Specify `write to file` Result
    Specify  in the unit test that the Result of the `write to file` use case must be a String

--- test/write_to_file_test.dart
+++ test/write_to_file_test.dart
@@ -5,9 +5,9 @@ import 'package:hledger_connector/src/write_to_file.dart';
 
 void main() {
   test('throwing error if the file name is empty', () async {
-    Result result = writeToFile(Success(value: ''), '');
-    expect(result.runtimeType, Error);
-    if (result is Error) {
+    Result<String> result = writeToFile(Success(value: ''), '');
+    expect(result.runtimeType, Error<String>);
+    if (result is Error<String>) {
       expect(result.message, 'The file name cannot be empty.');
     }
   });
