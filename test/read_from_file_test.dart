e7e176661abda07d7acfd2775678a7e8d8f72681
Klaus <thorres@brothersofgrey.net>
2026-06-12 12:47:54 +0200

    Remove async from test

--- test/read_from_file_test.dart
+++ test/read_from_file_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 import 'package:hledger_connector/src/read_from_file.dart';
 
 void main() {
-  test('throwing error if the file name is empty', () async {
+  test('throwing error if the file name is empty', () {
     Result<List<String>> result = readFromFile('');
     expect(result.runtimeType, Error<List<String>>);
     if (result is Error<List<String>>) {
--- test/write_to_file_test.dart
+++ test/write_to_file_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 import 'package:hledger_connector/src/write_to_file.dart';
 
 void main() {
-  test('throwing error if the file name is empty', () async {
+  test('throwing error if the file name is empty', () {
     Result<String> result = writeToFile(Success(value: ''), '');
     expect(result.runtimeType, Error<String>);
     if (result is Error<String>) {
