d2933644655f05cc7105c6b1435c01eae1c91869
Klaus <mail@centaurus22.de>
2026-06-18 14:03:27 +0200

    Check a list without data errors

--- test/check_transactions_test.dart
+++ test/check_transactions_test.dart
@@ -13,4 +13,19 @@ void main() {
     final result = checkTransactions(transactionResult);
     expect(result.runtimeType, Error<List<Transaction>>);
   });
+  test('Transaction list without data error', () {
+    List<Transaction> transactions = List.empty(growable: true);
+    transactions.add(
+      Transaction(
+        date: DateTime(2003, 12, 05),
+        subTransactions: [
+          SubTransaction(account: 'assets', amount: Amount(value: -5)),
+          SubTransaction(account: 'rent', amount: Amount(value: 5)),
+        ],
+      ),
+    );
+    final transactionResult = Success(value: transactions);
+    final result = checkTransactions(transactionResult);
+    expect(result.runtimeType, Success<List<Transaction>>);
+  });
 }
